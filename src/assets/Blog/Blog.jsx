472bc9da308158f2fe5e7c414a129ac1b67fd149
Jobayer Mannan <jobayermannan777@gmail.com>
2023-05-24 10:02:52 +0600

    Also Not found page has be done

--- src/assets/Blog/Blog.jsx
+++ src/assets/Blog/Blog.jsx
@@ -6,7 +6,7 @@ const Blog = () => {
       <h2 className="text-2xl font-bold mb-4">React Hooks in a Nutshell</h2>
 
       <div className="mb-4">
-        <h3 className="text-lg font-semibold mb-2">When should you use context API?</h3>
+        <h3 className="text-gradient bg-gradient-to-l from-red-600 ">When should you use context API?</h3>
         <p>
           You can use the Context API when you want to pass data through the component tree without having to pass props
           down manually at every level. For example, if you have a theme that you want to apply to your entire app, you
@@ -16,7 +16,7 @@ const Blog = () => {
       </div>
 
       <div className="mb-4">
-        <h3 className="text-lg font-semibold mb-2">What is a custom hook?</h3>
+        <h3 className="text-gradient bg-gradient-to-r from-teal-600 ">What is a custom hook?</h3>
         <p>
           A custom hook is a function that allows you to reuse stateful logic across multiple components. For example, if
           you have two components that both need to fetch data from an API and display it on the screen, you can create a
@@ -25,7 +25,7 @@ const Blog = () => {
       </div>
 
       <div className="mb-4">
-        <h3 className="text-lg font-semibold mb-2">What is useRef?</h3>
+        <h3 className="text-gradient bg-gradient-to-l  from-yellow-400">What is useRef?</h3>
         <p>
           useRef is a hook that allows you to create a mutable reference that persists across renders. For example, if you
           have an input field that you want to focus on when the component mounts, you can use useRef to create a
@@ -34,7 +34,7 @@ const Blog = () => {
       </div>
 
       <div className="mb-4">
-        <h3 className="text-lg font-semibold mb-2">What is useMemo?</h3>
+        <h3 className="text-gradient bg-gradient-to-r from-sky-600 ">What is useMemo?</h3>
         <p>
           useMemo is a hook that allows you to memoize expensive computations so that they are only re-computed when
           their dependencies change. For example, if you have a component that renders a list of items and each item has
--- src/assets/Components/NotFoundPage/NotFound.jsx
+++ src/assets/Components/NotFoundPage/NotFound.jsx
@@ -3,9 +3,9 @@ import React from 'react';
 const NotFound = () => {
   return (
     <div>
-       <div className="flex flex-col items-center justify-center h-screen bg-red-700">
-      <h1 className="text-6xl font-bold text-white mb-8">404 - Page Not Found</h1>
-      <p className="text-lg text-white">Oops! The page you are looking for does not exist.</p>
+       <div className="flex flex-col items-center justify-center h-screen bg-indigo-50">
+      <h1 className="text-6xl font-bold text-primary mb-8">404 - Page Not Found</h1>
+      <p className="text-lg text-blue">Oops! The page you are looking for does not exist.</p>
       <img
         src="https://giphy.com/static/img/error_pages/crying-cowbow-emoji.gif"
         alt="404 Not Found"
